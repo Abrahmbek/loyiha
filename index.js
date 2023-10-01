19ed9c346d56536eafbbdf15799ae5d16e1724e3
Abrahmbek <145168869+Abrahmbek@users.noreply.github.com>
2023-10-01 21:45:43 +0900

    feat: loyiha added

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { create } from 'express-handlebars';
+
+
+           
+
+const app = express();
+
+const hbs = create({
+    defaultLayout: 'main',
+    extname: 'hbs',
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+res.render('index');
+});
+
+app.get('/about', (req, res) => {
+   res.render('about');
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
